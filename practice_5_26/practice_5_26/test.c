f82b3a9fa5dc9c38377d93abf52c09f1d8b60d6f
CoderVodka <Vodka178395@gmail.com>
2023-05-28 08:57:37 +0800

    练

---
+++ practice_5_26/practice_5_26/test.c
@@ -0,0 +1,25 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//int main()
+//{
+//	int arr[] = {73,32,99,97,110,32,100,111,105,116,33};
+//	int i = 0;
+//	int size = sizeof(arr) / sizeof(arr[0]);     //计算数组元素个数。
+//	while (size > i)
+//	{
+//		printf("%c", arr[i]);
+//		i++;
+//	}
+//
+//	return 0;
+//}
+
+//初始化参数时0.0会被认为是double类型，使用0.0f表示float类型。eg.
+//int main()
+//{
+//	float math = 0.0f;
+//	return 0;
+//}
+
+
