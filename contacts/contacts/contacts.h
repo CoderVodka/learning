2fbbe7cbf40a67235436b1dda204ef8c282b1d75
CoderVodka <Vodka178395@gmail.com>
2023-08-12 19:42:09 +0800

    通讯录，进行了一些修改，将联系人数组data和计数count封装成一个结构体，方便维护和使用，并将data,name,sex,tele的数组长度定义成常量，方便修改

--- contacts/contacts/contacts.h
+++ contacts/contacts/contacts.h
@@ -4,18 +4,23 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define DATA 100
+#define NAME 20
+#define SEX 7
+#define TELE 12
+
 typedef struct PeoInfo
 {
-	char name[20];
+	char name[NAME];
 	int age;
-	char sex[7];
-	char tele[12];
+	char sex[SEX];
+	char tele[TELE];
 }PeoInfo;
 
 //创建通讯录需要记录成员数量，因此将通讯录数组与计数封装成一个结构体，更方便使用
 typedef struct Contacts
 {
-	PeoInfo data[100];
+	PeoInfo data[DATA];
 	int count;
 }Contacts;
 
